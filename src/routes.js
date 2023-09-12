18bcdf3959fe2065e2d81330b1fb323e3325ca0f
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-09-12 18:32:52 -0300

    feat: Single article listing route created successfully

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 function routes(app) {
   app.use('/get/news', require('./routes/getNews'));
+  app.use('/get/new/article', require('./routes/getIdNews'));
   app.use('/news/create', require('./routes/newsCreate'));
   return;
 }
--- src/routes/getIdNews.js
+++ src/routes/getIdNews.js
@@ -4,8 +4,18 @@ const connectBD = require('../middlewares/connectBD');
 const ARTICLES = require('../models/articles')
 
 
-router.get('/', connectBD, async function (req, res) {
+router.get('/:id', connectBD, async function (req, res) {
   try {
+    let idArticle = req.params.id;
+    const checkArticles = await ARTICLES.findOne({ _id: idArticle });
+
+    if (!checkArticles) throw new Error('Article not found!');
+
+    res.status(200).json({
+      status: "ok",
+      statusMessage: "Article listed successfully",
+      response: checkArticles,
+    });
 
   } catch (error) {
     return console.error(`Erro: ${error}`);
