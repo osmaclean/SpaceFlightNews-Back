10272bcfa9e32da1f051a3176429c42a8c576785
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-09-13 11:43:46 -0300

    fix: Modification to swagger tags again³

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 function routes(app) {
-  app.use('/get/news', require('./routes/getNews'));
   app.use('/get/new/article', require('./routes/getIdNews'));
   app.use('/delete/article', require('./routes/deleteNew'));
+  app.use('/get/news', require('./routes/getNews'));
   app.use('/news/create', require('./routes/newsCreate'));
   return;
 }
