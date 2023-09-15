3434af6f310fd6fdbe44ba82a789f183743b4762
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-09-15 20:38:35 -0300

    feat: Validation of unique articles in the news/create route carried out successfully

--- src/routes/newsCreate.js
+++ src/routes/newsCreate.js
@@ -13,8 +13,9 @@ router.post('/', connectBD, async function (req, res) {
     if (responseApi.status === 200 && responseApi.data.status == 'ok') {
       const dataRes = responseApi.data
       const articlesRes = dataRes.articles
+      const addedArticles = [];
 
-      articlesRes.forEach(element => {
+      for (const element of articlesRes) {
         let sourceId = element.source.id
         let sourceName = element.source.name
         let author = element.author
@@ -25,13 +26,18 @@ router.post('/', connectBD, async function (req, res) {
         let publishedAt = element.publishedAt
         let content = element.content
 
-        articleSchema.create({ sourceId, sourceName, author, title, description, url, urlToImage, publishedAt, content })
-      });
-      res.status(200).json({ status: 'ok', statusMessage: 'Articles added successfully' });
+        const existingArticle = await articleSchema.findOne({ title });
 
-    } else {
-      console.error('Não foi possível obter a resposta da requisição');
-    }
+        if (!existingArticle) {
+          await articleSchema.create({ sourceId, sourceName, author, title, description, url, urlToImage, publishedAt, content });
+          addedArticles.push(title);
+        }
+      };
+      if (addedArticles.length > 0) res.status(200).json({ status: 'ok', statusMessage: 'Articles added successfully', addedArticles });
+      else res.status(200).json({ status: 'ok', statusMessage: 'No new articles added' });
+
+
+    } else console.error('Não foi possível obter a resposta da requisição');
   }
   catch (error) {
     console.error(`Erro: ${error}`);
