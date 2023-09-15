6a2f5f2a0e756a81338b58a2a6f7e72f9fc7f263
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-09-15 19:38:06 -0300

    refactor: Changing the error response and deleting the findOne method

--- src/routes/deleteNew.js
+++ src/routes/deleteNew.js
@@ -8,9 +8,9 @@ router.delete('/:id', connectDB, async function (req, res) {
     // #swagger.tags = ['Article']
     // #swagger.description = "Endpoint to delete an article in the database."
     let idArticle = req.params.id;
-    const checkArticles = await article.findOne({ _id: idArticle }).deleteOne({ _id: idArticle });
+    const checkArticles = await article.deleteOne({ _id: idArticle });
 
-    if (!checkArticles) throw new Error('Article not found!');
+    if (!checkArticles) res.status(404).json({ message: "Article not found" });
 
     res.status(200).json({
       status: "ok",
